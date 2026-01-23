8626498888803b2bde927e1c466364132e1cac9c
M A MUKTADEER <muktadeer.ma@gmail.com>
2026-01-23 16:25:57 +0600

    ok

--- v2/ithouse-v2/src/test/java/com/ithouse/core/anotations/ItHouseDBValueTest.java
+++ v2/ithouse-v2/src/test/java/com/ithouse/core/anotations/ItHouseDBValueTest.java
@@ -6,7 +6,6 @@ import com.ithouse.core.anotations.provider.ItHouseConfigProvider;
 import com.ithouse.core.anotations.services.ConfigRefreshService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
-import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
